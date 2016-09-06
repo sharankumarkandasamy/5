535768a28b791e23d6cac71e0ac4e49ba5753747
sharankumarkandasamy <sharankumar29.2@gmail.com>
2016-09-06 21:35:20 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,15 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class reverse 
+{
+	public static void main(String[] args) 
+	{
+		  String string="sharan";
+	      String reverse = new StringBuffer(string).
+	      reverse().toString();
+	   
+	      System.out.println(" "+reverse);
+	}
+}
